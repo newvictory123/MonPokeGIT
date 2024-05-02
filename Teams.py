474f24eca7bd434b82e90b54433b5b283b740a86
aebletaerte-RAP1D <118903850+aebletaerte-RAP1D@users.noreply.github.com>
2024-05-02 13:03:04 +0200

    Update Teams.py

--- Teams.py
+++ Teams.py
@@ -14,8 +14,22 @@ ChuPika = MonPoke("ChuPika", 108, "fire", 30, 78, 100, 100, 100)
 
 LetDig = MonPoke("LetDig", 108, "ground", 30, 78, 100, 100, 100, level = 40)
 
+Saurvenu = MonPoke.MonPoke("SaurVenu", 108, "Grass", 30, 78, 100, 100, 100)
+
+Ianpers = MonPoke.MonPoke("Ianpers", 108, "Normal", 30, 78, 100, 100, 100)
+
+Zonemagne = MonPoke.MonPoke("Zonemagne", 108, "Steel", 30, 78, 100, 100, 100)
+
+Ableclef = MonPoke.MonPoke("Ableclef", 108, "Fairy", 30, 78, 100, 100, 100)
+
+Rialta = MonPoke.MonPoke("Rialta", 108, "Dragon", 30, 78, 100, 100, 100)
+
+Lixteel = MonPoke.MonPoke("Lixteel", 108, "Steel", 30, 78, 100, 100, 100)
+
 ###Premade teams
 team1 = (LetDig, ChuPika, Zardichar)
+team2 = (Saurvenu, Ianpers, Zonemagne)
+team3 = (Ableclef, Rialta, Lixteel)
 
 class Team(enum.Enum):
         
