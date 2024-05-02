a92fdb6f7c202f7497945e7dc4d8a4ac5d612c13
aebletaerte-RAP1D <118903850+aebletaerte-RAP1D@users.noreply.github.com>
2024-05-02 12:59:57 +0200

    Update MonPokeTeams.py

--- MonPokeTeams.py
+++ MonPokeTeams.py
@@ -1,11 +1,19 @@
 import MonPoke
 
 ###singular MonPokes
-Zardichar = MonPoke.MonPoke("Zardichar", 108, "grass", 30, 78, 100, 100, 100)
+Zardichar = MonPoke.MonPoke("Zardichar", 108, "Fire", 30, 78, 100, 100, 100)
 
-ChuPika = MonPoke.MonPoke("ChuPika", 108, "fire", 30, 78, 100, 100, 100)
+ChuPika = MonPoke.MonPoke("ChuPika", 108, "Electric", 30, 78, 100, 100, 100)
 
 LetDig = MonPoke.MonPoke("LetDig", 108, "ground", 30, 78, 100, 100, 100)
 
+Saurvenu = MonPoke.MonPoke("SaurVenu", 108, "Grass", 30, 78, 100, 100, 100)
+
+Ianpers = MonPoke.MonPoke("Ianpers", 108, "Normal", 30, 78, 100, 100, 100)
+
+Zonemagne = MonPoke.MonPoke("Zonemagne", 108, "Steel", 30, 78, 100, 100, 100)
+
 ###Premade teams
 team1 = (LetDig, ChuPika, Zardichar)
+
+team2 = (Saurvenu, Ianpers, Zonemagne)
