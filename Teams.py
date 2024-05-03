ea09b16002330bd1b95efe159138359bfd536ea5
aebletaerte-RAP1D <118903850+aebletaerte-RAP1D@users.noreply.github.com>
2024-05-03 11:09:05 +0200

    Update Teams.py

--- Teams.py
+++ Teams.py
@@ -6,33 +6,37 @@ from Match import match
 from discord import app_commands
 from discord.ext import commands
 from MonPoke import MonPoke
+from Moves import Moves
+
+###moves
+Tackle = Moves("Tackle", "Normal", "Physical", 40, 100)
 
 ###singular MonPokes
-Zardichar = MonPoke("Zardichar", 108, "grass", 30, 78, 100, 100, 100)
+Zardichar = MonPoke("Zardichar", 108, "grass", 30, 78, 100, 100, 100, [Tackle])
 
-ChuPika = MonPoke("ChuPika", 108, "fire", 30, 78, 100, 100, 100)
+ChuPika = MonPoke("ChuPika", 108, "fire", 30, 78, 100, 100, 100, [Tackle])
 
-LetDig = MonPoke("LetDig", 108, "ground", 30, 78, 100, 100, 100, level = 40)
+LetDig = MonPoke("LetDig", 108, "ground", 30, 78, 100, 100, 100, [Tackle])
 
-Saurvenu = MonPoke.MonPoke("SaurVenu", 108, "Grass", 30, 78, 100, 100, 100)
+SaurVenu = MonPoke.MonPoke("SaurVenu", 108, "Grass", 30, 78, 100, 100, 100, [Tackle])
 
-Ianpers = MonPoke.MonPoke("Ianpers", 108, "Normal", 30, 78, 100, 100, 100)
+Ianpers = MonPoke.MonPoke("Ianpers", 108, "Normal", 30, 78, 100, 100, 100, [Tackle])
 
-Zonemagne = MonPoke.MonPoke("Zonemagne", 108, "Steel", 30, 78, 100, 100, 100)
+ZoneMagne = MonPoke.MonPoke("Zonemagne", 108, "Steel", 30, 78, 100, 100, 100, [Tackle])
 
-Ableclef = MonPoke.MonPoke("Ableclef", 108, "Fairy", 30, 78, 100, 100, 100)
+Ableclef = MonPoke.MonPoke("Ableclef", 108, "Fairy", 30, 78, 100, 100, 100, [Tackle])
 
-Rialta = MonPoke.MonPoke("Rialta", 108, "Dragon", 30, 78, 100, 100, 100)
+RiAlta = MonPoke.MonPoke("Rialta", 108, "Dragon", 30, 78, 100, 100, 100, [Tackle])
 
-Lixteel = MonPoke.MonPoke("Lixteel", 108, "Steel", 30, 78, 100, 100, 100)
+LixTeel = MonPoke.MonPoke("Lixteel", 108, "Steel", 30, 78, 100, 100, 100, [Tackle])
 
 ###Premade teams
 team1 = (LetDig, ChuPika, Zardichar)
-team2 = (Saurvenu, Ianpers, Zonemagne)
-team3 = (Ableclef, Rialta, Lixteel)
+team2 = (SaurVenu, Ianpers, ZoneMagne)
+team3 = (Ableclef, RiAlta, LixTeel)
 
 class Team(enum.Enum):
         
             LetDig_ChuPika_Zardichar = team1
-            banana = 2
-            cherry = 35
+            SaurVenu_IanPers_ZoneMagne = team2
+            AbleClef_RiAlta_LixTeel = team3
