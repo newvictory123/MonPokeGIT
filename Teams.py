ad304a70204f6ecb0cb2bf3a7486f93507dcd204
aebletaerte-RAP1D <118903850+aebletaerte-RAP1D@users.noreply.github.com>
2024-05-14 10:15:02 +0200

    .

--- Teams.py
+++ Teams.py
@@ -11,6 +11,7 @@ ChuPika = MonPoke("ChuPika", 108, "fire", 30, 78, 100, 100, 100,{"Tackle":Tackle
 
 LetDig = MonPoke("LetDig", 108, "ground", 30, 78, 100, 100, 100,{"Tackle":Tackle})
 
+<<<<<<< Updated upstream
 Saurvenu = MonPoke("SaurVenu", 108, "Grass", 30, 78, 100, 100, 100,{"Tackle":Tackle})
 
 Ianpers = MonPoke("Ianpers", 108, "Normal", 30, 78, 100, 100, 100,{"Tackle":Tackle})
@@ -27,6 +28,24 @@ Lixteel = MonPoke("Lixteel", 108, "Steel", 30, 78, 100, 100, 100,{"Tackle":Tackl
 team1 = [LetDig, ChuPika, Zardichar]
 team2 = [Saurvenu, Ianpers, Zonemagne]
 team3 = [Ableclef, Rialta, Lixteel]
+=======
+SaurVenu = MonPoke("SaurVenu", 108, "Grass", 30, 78, 100, 100, 100, [Tackle])
+
+Ianpers = MonPoke("Ianpers", 108, "Normal", 30, 78, 100, 100, 100, [Tackle])
+
+ZoneMagne = MonPoke("Zonemagne", 108, "Steel", 30, 78, 100, 100, 100, [Tackle])
+
+Ableclef = MonPoke("Ableclef", 108, "Fairy", 30, 78, 100, 100, 100, [Tackle])
+
+RiaAlta = MonPoke("Rialta", 108, "Dragon", 30, 78, 100, 100, 100, [Tackle])
+
+LixTeel = MonPoke("Lixteel", 108, "Steel", 30, 78, 100, 100, 100, [Tackle])
+
+###Premade teams
+team1 = (LetDig, ChuPika, Zardichar)
+team2 = (SaurVenu, Ianpers, ZoneMagne)
+team3 = (Ableclef, RiaAlta, LixTeel)
+>>>>>>> Stashed changes
 
 class Team(enum.Enum):
             LetDig_ChuPika_Zardichar = team1
