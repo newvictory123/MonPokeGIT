56cb3ec91599fad7c1aa733daf34144d4a308e15
newvictory123 <viktoremilreimannjensen@gmail.com>
2024-05-15 11:15:57 +0200

    return

--- MonPoke.py
+++ MonPoke.py
@@ -6,23 +6,25 @@ class MonPoke:
         self.Name = Name
         self.HP = self.Health(HP)
         self.Type = Type
+        self.level = level
         self.Speed = self.Otherstat(Speed)
         self.Attack = self.Otherstat(Attack)
         self.Defense = self.Otherstat(Defense)
         self.SpAtk = self.Otherstat(SpAtk)
         self.SpDef = self.Otherstat(SpDef)
         self.moves = moves
-        self.level = level
         self.dead = dead
 
 
 
     def Health(self, Base):
         finalHP= ((((2 * Base + random.randint(1,31) + (85/4))*50)/100)+10+50)
+        return finalHP
         print(finalHP)
 
     def Otherstat(self, stat):
         finalstat = (((((2*stat) + random.randint(0, 31) +(85/4))*self.level)/100)+5)
+        return finalstat
         print(finalstat)
 
 """HP(108, 50, 74, random.randint(1,31))"""
