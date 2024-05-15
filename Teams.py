497427b63bca2e63370589777c844a07bc398cd4
aebletaerte-RAP1D <118903850+aebletaerte-RAP1D@users.noreply.github.com>
2024-05-15 19:18:42 +0200

    Update Teams.py

--- Teams.py
+++ Teams.py
@@ -41,9 +41,9 @@ SlashNight = Moves("Slash Night", "Dark", "Physical", 80, 100)
 ScissorX = Moves("Scissor X", "Bug", "Physical", 80, 100)
 
 ###singular MonPokes
-Zardichar = MonPoke("Zardichar", 10, "Grass", 30, 78, 100, 100, 100,{"Tackle":Tackle, "ThrowerFlame": ThrowerFlame, "Break Bricks": BreakBricks, "Bird Brave": BirdBrave})
+Zardichar = MonPoke("Zardichar", 10, "Fire", 30, 78, 100, 100, 100,{"Tackle":Tackle, "ThrowerFlame": ThrowerFlame, "Break Bricks": BreakBricks, "Bird Brave": BirdBrave})
 
-ChuPika = MonPoke("ChuPika", 10, "Fire", 30, 78, 100, 100, 100,{"Tackle":Tackle, "BoltThunder": BoltThunder, "Frus": Frus, "Ball Gyro": BallGyro})
+ChuPika = MonPoke("ChuPika", 10, "Electric", 30, 78, 100, 100, 100,{"Tackle":Tackle, "BoltThunder": BoltThunder, "Frus": Frus, "Ball Gyro": BallGyro})
 
 LetDig = MonPoke("LetDig", 10, "Ground", 30, 78, 100, 100, 100,{"Tackle":Tackle, "QuakeEarth": QuakeEarth, "Slide Rocks": SlideRocks, "Tackle PLUS": TacklePLUS})
 
